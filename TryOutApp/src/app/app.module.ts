dccdef8b3bc2a971a523da9a9645ea990fd717cf
Filipe Silva <filipe.silva@ad.infosys.com>
2021-12-20 15:54:43 -0500

    two way data binding

---
+++ TryOutApp/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+
+@NgModule({
+  declarations: [AppComponent, TwoWayBindingComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
