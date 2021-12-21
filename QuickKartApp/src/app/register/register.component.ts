00d4e421d3009483fb9240c9abf2d79ae267b19d
Filipe Silva <filipe.silva@ad.infosys.com>
2021-12-21 10:24:37 -0500

    reactive forms validation

--- QuickKartApp/src/app/register/register.component.ts
+++ QuickKartApp/src/app/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -15,18 +20,34 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       emailId: ['', [Validators.required, Validators.minLength(12)]],
-      gender: [''],
-      password: [''],
-      dateOfBirth: [''],
-      address: [''],
+      gender: ['', Validators.required],
+      password: ['', Validators.required],
+      dateOfBirth: [new Date(), checkDate],
+      address: ['', Validators.required],
     });
   }
 
   submitForm() {
+    console.log(this.registerForm.value);
     if (this.registerForm.valid) {
       this.msg = 'Signup Successful';
     } else {
       this.msg = 'Try again Later';
     }
+  }
+}
+
+function checkDate(control: FormControl) {
+  const currentDate = new Date();
+  const givenDate = new Date(control.value);
+
+  if (givenDate <= currentDate || givenDate == null) {
+    return null;
+  } else {
+    return {
+      dateError: {
+        message: "Enter a date less than today's date",
+      },
+    };
   }
 }
